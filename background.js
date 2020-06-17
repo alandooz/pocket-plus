ef049b9305165927170e844cddbefa47c2e5b681
Alan Becker Capuya <alan@beckercapuya.com>
2020-06-17 22:30:54 +0000

    init testing

---
+++ background.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+//  var button = document.getElementById("submit")
+console.log('HOLA')
+//  button.addEventListener("click", (e) => {
+//    console.log(e)
+//  })
+})
